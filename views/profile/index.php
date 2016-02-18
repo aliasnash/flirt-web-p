3021b57c0186399accf8b2c6f84ddffbd099a83a
nash <den.transfer@gmail.com>
2016-02-18 08:21:20 +0300

    edit

--- views/profile/index.php
+++ views/profile/index.php
@@ -3,8 +3,7 @@
 
 	<div class="row">
 		<div class="col-md-2 col-md-offset-5 col-xs-12">
-			<a href="#" class="thumbnail">
-				<img class="img-responsive" src="<?=WEB_APP?>/images/<?='00.jpg';?>" alt="">
+			<a href="#" class="thumbnail"> <img class="img-responsive" src="<?=WEB_APP?>/images/<?='00.jpg';?>" alt="">
 			</a>
 		</div>
 
@@ -34,16 +33,86 @@
 		</div>
 	</div>
 	<div class="row">
+		<div id="links">
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/01.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/01.jpg" alt="Banana" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/02.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/02.jpg" alt="Apple" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/03.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/03.jpg" alt="Orange" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/04.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/04.jpg" alt="Orange" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/05.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/05.jpg" alt="Orange" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/06.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/06.jpg" alt="Orange" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/07.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/07.jpg" alt="Orange" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/01.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/01.jpg" alt="Orange" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/09.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/09.jpg" alt="Orange" title="...">
+				</a>
+			</div>			
+		</div>
+	</div>
+
+	<div class="row">
 		<div class="form-group col-md-12 col-xs-12 text-center">
 			<div class="btn-group">
-				<a href="<?=WEB_APP?>/profile/edit" class="btn btn-info btn-lg">
-					<i class="fa fa-pencil-square-o fa-lg"></i> Изменить
+				<a href="<?=WEB_APP?>/profile/edit" class="btn btn-info btn-lg"> <i class="fa fa-pencil-square-o fa-lg"></i> Изменить
 				</a>
 			</div>
 		</div>
 	</div>
 </div>
 
+<div id="blueimp-gallery" class="blueimp-gallery " data-use-bootstrap-modal="false">
 
+	<!-- The container for the modal slides -->
+	<div class="slides"></div>
+
+	<!-- Controls for the borderless lightbox -->
+	<h3 class="title"></h3>
+	<a class="prev">‹</a> <a class="next">›</a> <a class="close">×</a> <a class="play-pause"></a>
+	<ol class="indicator"></ol>
+
+	<!-- The modal dialog, which will be used to wrap the lightbox content -->
+	<div class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" aria-hidden="true">&times;</button>
+					<h4 class="modal-title"></h4>
+				</div>
+				<div class="modal-body next"></div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default pull-left prev">
+						<i class="glyphicon glyphicon-chevron-left"></i>
+					</button>
+					<button type="button" class="btn btn-primary next">
+						<i class="glyphicon glyphicon-chevron-right"></i>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 
--- views/users/index.php
+++ views/users/index.php
@@ -3,8 +3,7 @@
 
 	<div class="row">
 		<div class="col-md-2 col-md-offset-5 col-xs-12">
-			<a href="#" class="thumbnail">
-				<img class="img-responsive" src="<?=WEB_APP?>/images/<?='00.jpg';?>" alt="">
+			<a href="#" class="thumbnail"> <img class="img-responsive" src="<?=WEB_APP?>/images/<?='00.jpg';?>" alt="">
 			</a>
 		</div>
 
@@ -33,8 +32,80 @@
 			<p class="text-muted"><?=$profile['description'];?></p>
 		</div>
 	</div>
+
+	<div class="row">
+		<div id="links">
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/01.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/01.jpg" alt="Banana" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/02.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/02.jpg" alt="Apple" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/03.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/03.jpg" alt="Orange" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/04.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/04.jpg" alt="Orange" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/05.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/05.jpg" alt="Orange" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/06.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/06.jpg" alt="Orange" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/07.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/07.jpg" alt="Orange" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/01.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/01.jpg" alt="Orange" title="...">
+				</a>
+			</div>
+			<div class="col-md-2 col-xs-2 new-padding">
+				<a href="<?=WEB_APP?>/images/09.jpg" class="thumbnail" data-gallery> <img class="img-responsive" src="<?=WEB_APP?>/images/09.jpg" alt="Orange" title="...">
+				</a>
+			</div>
+		</div>
+	</div>
 </div>
 
+<div id="blueimp-gallery" class="blueimp-gallery " data-use-bootstrap-modal="false">
+
+	<!-- The container for the modal slides -->
+	<div class="slides"></div>
+
+	<!-- Controls for the borderless lightbox -->
+	<h3 class="title"></h3>
+	<a class="prev">‹</a> <a class="next">›</a> <a class="close">×</a> <a class="play-pause"></a>
+	<ol class="indicator"></ol>
+
+	<!-- The modal dialog, which will be used to wrap the lightbox content -->
+	<div class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" aria-hidden="true">&times;</button>
+					<h4 class="modal-title"></h4>
+				</div>
+				<div class="modal-body next"></div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default pull-left prev">
+						<i class="glyphicon glyphicon-chevron-left"></i>
+					</button>
+					<button type="button" class="btn btn-primary next">
+						<i class="glyphicon glyphicon-chevron-right"></i>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 
 
