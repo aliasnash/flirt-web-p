b0a4976516ba9529bd4385fb0fb56357cecb0abe
nash <den.transfer@gmail.com>
2016-02-23 14:22:17 +0300

    dd

--- views/index/test.php
+++ views/index/test.php
@@ -1,49 +1,27 @@
 <div class="panel panel-default">
 	<div class="lead text-center heading">TEST</div>
 	<div class="panel-body">
-		<ul class="media-list">
-		<?php foreach($messages as $key => $value) {?>
-			<?php if($value['msgowner']) {?>
-			<li class="media">
-				<div class="media-body bg-info">
-					<div class="media">
-						<p class="pull-right" style="margin-bottom: 0px;">
-							<img class="media-object img-circle " style="width: 60px;" src="<?=WEB_APP?>/images/<?=$value['pspath'];?>" />
-						</p>
-						<div class="media-body text-right">
-							<?=$value['message'];?> <br /> <small class="text-muted"><?=$value['dateadded'];?></small>
-						</div>
-					</div>
-				</div>
-			</li>				
-			<?php }else{?>
-			<li class="media">
-				<div class="media-body bg-warning">
-					<div class="media">
-						<a class="pull-left" href="<?=WEB_APP?>/users/profile?id=<?=$value['idus'];?>">
-							<img class="media-object img-circle " style="width: 60px;" src="<?=WEB_APP?>/images/<?=$value['pspath'];?>" />
-						</a>
-						<div class="media-body text-left">
-							<?=$value['message'];?> <br /> <small class="text-muted"><?=$value['dateadded'];?></small>
-						</div>
-					</div>
-				</div>
-			</li>
-			<?php }?>
-		<?php }?>
-		</ul>
+		<p class="foo" style="">
+			<img src="<?=WEB_APP?>/images/01.jpg" />
+		</p>
+		<p class="foo" style="">
+			<img src="<?=WEB_APP?>/images/02.jpg" />
+		</p>
+		<p class="foo" style="">
+			<img src="<?=WEB_APP?>/images/03.jpg" />
+		</p>
 	</div>
 
 	<div class="panel-footer">
-		<form class="form" role="form" method="post">
-			<div class="input-group">
-				<input type="text" class="form-control" id="msg" name="msg" placeholder="Сообщение..." />
-				<span class="input-group-btn">
-					<button class="btn btn-info" type="submit">
-						<i class="fa fa fa-paper-plane-o fa-lg"></i>
-					</button>
-				</span>
-			</div>
+
+		<form method="post">
+			<input type="hidden" name="x1" value="" />
+			<input type="hidden" name="y1" value="" />
+			<input type="hidden" name="x2" value="" />
+			<input type="hidden" name="y2" value="" />
+			<input type="hidden" name="w" value="" />
+			<input type="hidden" name="h" value="" />
+			<input type="submit" name="submit" value="Submit" />
 		</form>
 	</div>
 </div>
