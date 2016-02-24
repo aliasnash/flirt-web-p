013c88148573700d98e5c7caf22496bffa585a19
nash <den.transfer@gmail.com>
2016-02-24 23:44:20 +0300

    edit

--- controllers/photo.php
+++ controllers/photo.php
@@ -2,50 +2,80 @@
 
 class Controller_Photo extends Controller_Base {
 
-    function index() {
-        $idu = isset($_SESSION['idu']) ? $_SESSION['idu'] : 0;
-        
-//         $model = new Model_Profile();
-//         $profiles = $model->getProfile($idu);
-        
-//         if (count($profiles) > 0) {
-//             $profile = $profiles[0];
-//         }
-        
-//         if (!empty($profile)) {
-//             $photos = $model->getPhotoList($idu);
-            
-//             $this->template->vars('photos', $photos);
-//             $this->template->vars('profile', $profile);
-            $this->template->view('index', $idu > 0);
-//         } else {
-//             unset($_SESSION['idu']);
-//             header('Location:' . WEB_APP);
-//         }
-    }
+	function index() {
+		$idu = isset($_SESSION['idu']) ? $_SESSION['idu'] : 0;
+		
+		if ($idu > 0) {
+			$this->template->view('index', true);
+		} else {
+			unset($_SESSION['idu']);
+			header('Location:' . WEB_APP);
+		}
+	}
 
-    function confirm() {
-        $idu = isset($_SESSION['idu']) ? $_SESSION['idu'] : 0;
-        
-//         $_FILES['add-photo']
-        
-//         $model = new Model_Profile();
-//         $profiles = $model->getProfile($idu);
-        
-//         if (count($profiles) > 0) {
-//             $profile = $profiles[0];
-//         }
-        
-//         $monthList = array(1 => 'Январь', 2 => 'Февраль', 3 => 'Март', 4 => 'Апрель', 5 => 'Май', 6 => 'Июнь', 7 => 'Июль', 8 => 'Август', 9 => 'Сентябрь', 10 => 'Октябрь', 11 => 'Ноябрь', 12 => 'Декабрь');
-        
-//         if (!empty($profile)) {
-//             $this->template->vars('cities', $model->getCityList());
-//             $this->template->vars('profile', $profile);
-//             $this->template->vars('monthList', $monthList);
-//             $this->template->view('edit', $idu > 0);
-//         } else {
-//             unset($_SESSION['idu']);
-//             header('Location:' . WEB_APP);
-//         }
-    }
+	function upload() {
+		$idu = isset($_SESSION['idu']) ? $_SESSION['idu'] : 0;
+		
+		if ($idu > 0) {
+			
+			if (isset($_FILES['add-photo'])) {
+				var_dump($_FILES['add-photo']);
+				
+				$tmpFile = $_FILES['add-photo']["tmp_name"];
+				
+				$info = getimagesize($tmpFile);
+				
+				echo ("<BR>");
+				print_r($info);
+				
+				
+				list($width, $height, $mime) = getimagesize($tmpFile);
+				if ($width == null && $height == null) {
+					// header('Location:' . WEB_APP);
+					return;
+				} else {
+					$fileName = 'c:/1.jpg';
+					
+					echo ("<BR>.mime:$mime");
+					
+					if ($mime == 'image/jpeg')
+						$image = imagecreatefromjpeg($tmpFile);
+					elseif ($mime == 'image/gif')
+						$image = imagecreatefromgif($tmpFile);
+					elseif ($mime == 'image/png')
+						$image = imagecreatefrompng($tmpFile);
+					
+					echo ("<BR>.m:$image");
+					
+					$side = min($width, $height);
+					// $side = min($side, 600);
+					
+					echo ("<BR>.side:$side");
+					
+					$rect = array('x' => 0, 'y' => 0, 'width' => $side, 'height' => $side);
+					$image = imagecrop($image, $rect);
+					echo ("<BR>.m:$image");
+					
+					imagejpeg($image, $fileName, 80);
+					
+					list($width, $height) = getimagesize($fileName);
+					
+					echo ("<BR>.w:$width");
+					echo ("<BR>.h:$height");
+					
+					// $image = new Imagick("d:/develop/eclipse workspace/Eclipse PHP Learning/flirt/images/23.jpg");
+					// // $image = new Imagick($tmpFile);
+					
+					// $image->thumbnailImage(400, 400);
+					// $image->writeImage($fileName);
+				}
+			}
+			
+			// header('Location:' . WEB_APP . '/profile/');
+		} else {
+			unset($_SESSION['idu']);
+			header('Location:' . WEB_APP);
+		}
+	
+	}
 }
--- views/photo/index.php
+++ views/photo/index.php
@@ -4,7 +4,7 @@
 	<div class="row">
 		<div class="col-md-12 col-xs-12 text-center">
 			<div id="add-photo-errors" class="center-block" style="display: none"></div>
-			<form class="text-center" action="<?=WEB_APP?>/photo/confirm" method="post" enctype="multipart/form-data">
+			<form class="text-center" action="<?=WEB_APP?>/photo/upload" method="post" enctype="multipart/form-data">
 				<div class="add-photo-box center-block" style="width: 180px">
 					<input id="add-photo" name="add-photo" type="file" class="file-loading">
 				</div>
